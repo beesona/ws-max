c879bae035353f522ad9a18ec3a1d64082072324
Tyler Andor <tyandor@gmail.com>
2018-02-07 11:18:26 -0700

    updates new borrower model

--- src/app/models/borrowernew.ts
+++ src/app/models/borrowernew.ts
@@ -1,5 +1,5 @@
 export interface IAddress {
-    Id?: any;
+    id?: any;
     city: string;
     type?: string;
     state: string;
@@ -19,26 +19,26 @@ export interface IEmailAddress {
     emailAddress: string;
 }
 
-export interface Account {
-    AccountNumber: string;
-    IsActive: boolean;
-    ResultStatus: string;
-    SystemName: string;
-    LastUpdated: Date;
-    CurrentSchoolEnrollment: ICurrentSchoolEnrollment;
-    MaximumDaysPastDue: number;
-    DelinquentAmount: number;
-    OutstandingPrincipalBalance: number;
-    OutstandingInterest: number;
-    OutstandingFees: number;
-    TenDayPayoffAmount: number;
-    TotalBalance: number;
-    RegularPaymentAmount: number;
-    LastPaymentDate: Date;
-    LastPaymentAmount: number;
-    CurrentPaymentAmountDue: number;
-    DailyInterestAmount: number;
-}
+// export interface Account {
+//   AccountNumber: string;
+//   IsActive: boolean;
+//   ResultStatus: string;
+//   SystemName: string;
+//   LastUpdated: Date;
+//   CurrentSchoolEnrollment: ICurrentSchoolEnrollment;
+//   MaximumDaysPastDue: number;
+//   DelinquentAmount: number;
+//   OutstandingPrincipalBalance: number;
+//   OutstandingInterest: number;
+//   OutstandingFees: number;
+//   TenDayPayoffAmount: number;
+//   TotalBalance: number;
+//   RegularPaymentAmount: number;
+//   LastPaymentDate: Date;
+//   LastPaymentAmount: number;
+//   CurrentPaymentAmountDue: number;
+//   DailyInterestAmount: number;
+// }
 
 export interface IBorrower {
     borrowerId: any;
@@ -49,39 +49,15 @@ export interface IBorrower {
     lastName: string;
     ssn: string;
     dob: Date;
+    activeMilitary: boolean;
+    activePrivacy: boolean;
+    createdDate: Date;
+    updatedDate: Date;
+    externalReferenceId: string;
     address: IAddress;
     addresses: IAddress[];
     phones: IPhone[];
     emailAddresses: IEmailAddress[];
-    account: Account;
-    accounts: Account[];
-    Groups?: any;
-    Loans?: any;
-    Notes?: any;
-    Verifications: any[];
-    IsLegalStatus: boolean;
-    CpcFlag: string;
-    AddressStatus: string;
-    PhoneStatus: string;
-    NewBorrowerFlag?: any;
-    ECorrFlag: boolean;
-    ECorrOutOfSync: boolean;
-    FederalDefermentEligibility: any[];
-    FederalDefermentEligibilityOutOfSync: boolean;
-    KwikPayFlag: boolean;
-    KwikPayOutOfSync: boolean;
-    MmaUsername?: any;
-    MmaLastLogin: Date;
-    LastDemographicVerificationDate?: any;
-    LastDemographicVerificationDateOutOfSync: boolean;
-    RefreshedDate: Date;
-    BorrowerBeaconId?: any;
-    SystemName: string;
-    EntityState?: any;
-    ResultStatus?: any;
-    LastUpdated: Date;
-    EmailInSync: boolean;
-    EnrollmentStatusInSync: boolean;
-    SchoolInSync: boolean;
-    Messages: any[];
+    // account: Account;
+    // accounts: Account[];
 }

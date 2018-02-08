c5aa9957d8785136f8aa072f5892e44c64f73198
Tyler Andor <tyandor@gmail.com>
2018-02-08 13:15:44 -0700

    initial build of account component

--- src/app/feature/account/account.component.ts
+++ src/app/feature/account/account.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Subscription }   from 'rxjs/Subscription';
-
-import { AccountService } from '../../services/account.service'
-import { MessageService } from '../../services/message.service'
-import { IAccount, IGroup, ILoan } from '../../models/account';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -12,32 +7,27 @@ import { IAccount, IGroup, ILoan } from '../../models/account';
 })
 export class AccountComponent implements OnInit {
 
-  @Input() data: any;
-  subscription: Subscription;
-  account: IAccount;
-  groups: IGroup[];
-  loans: ILoan[];
-  constructor(private _acctSvc: AccountService, 
-    private _msgSvc: MessageService) { }
+  _account: string;
+  get account(): string {
+      this._account = value;
+  }
+  
+  @Input('account')
+  set account(value:string){
+      this.account = value;
+  }
 
-  ngOnInit() {
-    if (this._acctSvc.storedGroupsAndLoans != undefined){
-      this.account = this._acctSvc.storedGroupsAndLoans;
-      this.groups = this._acctSvc.storedGroupsAndLoans.Groups;
-      this.loans = this._acctSvc.storedGroupsAndLoans.Loans;
-    }else{
-      if (!!this._msgSvc.storedSearchSsn){
-        this._acctSvc.setGroupsAndLoans(this._msgSvc.storedSearchSsn);
-      }
-    } 
-    this.subscription = this._acctSvc.groupsAndLoans$.subscribe(
-      acct => {        
-        if (acct) {
-          this.account = acct;
-          this.groups = acct.Groups;
-          this.loans = acct.Loans;
-        }
-      })
+  //vape details. this will later be provided by a service
+  vape = false;
+
+  //interaction details. this will later be provided by a service
+  callType: string = 'INBOUND CALL';
+  callerName: string = 'ADAM BEESON';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.vape = true;
   }
 
 }
